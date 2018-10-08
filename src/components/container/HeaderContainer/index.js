195852d817bdaf6f0e84df51cb75f29229e25524
Boris Vujicic <turshija@gmail.com>
2018-10-08 20:49:18 +0200

    Add header

---
+++ src/components/container/HeaderContainer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HeaderContainer extends Component {
+
+  render() {
+    return (
+      <div styleName='header'>
+        <span>&lt;</span>
+        Search
+      </div>
+    );
+  }
+}
+export default HeaderContainer;
---
+++ src/pages/playlist/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class PlaylistPage extends Component {
+  render() {
+    return (
+      <div>Playlist</div>
+    );
+  }
+}
+export default PlaylistPage;
