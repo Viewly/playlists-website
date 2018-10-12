015cd6545ab26073a280ba8eefc4ac65df77d177
Nenad Jelovac <jelowack@gmail.com>
2018-10-13 00:24:05 +0200

    Add video thumbnails and trimmed titles

--- src/pages/playlist/components/video.js
+++ src/pages/playlist/components/video.js
@@ -3,13 +3,18 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Video = ({ id, title, description, playlist_id, thumbnail_url }) => (
-  <div className='o-grid__cell u-1/2@medium u-1/3@large u-1/4@extralarge u-margin-bottom'>
-  <Link to={`/player/${playlist_id}/${id}`}>
-    <img src={thumbnail_url} />
-    <div>
-      <h5>{title}</h5>
-    </div>
-  </Link>
+  <div className='o-grid__cell u-1/2@medium u-1/3@large u-1/4@extralarge'>
+    <Link className='c-video' to={`/player/${playlist_id}/${id}`}>
+      <div>
+        <div class="c-thumbnail">
+          <div class="c-thumbnail__link" href=""></div>
+          <img class="c-thumbnail__img js-thumbnail-img" src={thumbnail_url} />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-poster="" class="c-thumbnail__play-icon"><path fill="currentColor" d="M1.425.35L14.575 8l-13.15 7.65V.35z"></path></svg>
+          <span class="c-thumbnail__duration-indicator">6:55</span>
+        </div>
+        <h4 className='c-video__title'>{title}</h4>
+      </div>
+    </Link>
   </div>
 )
 
