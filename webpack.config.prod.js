65269dca1fe5460d2bafd6c9c44eab25dffca95d
Boris Vujicic <turshija@gmail.com>
2018-10-12 16:19:10 +0200

    Fix the build having missing index.html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ const baseConfig = require('./webpack.config');
 const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = Object.assign(baseConfig, {
   output: {
@@ -9,6 +10,10 @@ module.exports = Object.assign(baseConfig, {
     filename: "[name]-[hash].js"
   },
   plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new MiniCssExtractPlugin({
       filename: "style-[hash].css",
