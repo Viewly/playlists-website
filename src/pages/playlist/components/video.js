e7d506b2c11ffcd88b6f8720b1fb3c6481cbfa7a
Nenad Jelovac <jelowack@gmail.com>
2018-10-13 11:22:05 +0200

    Update play icon and add better hover transition

--- src/pages/playlist/components/video.js
+++ src/pages/playlist/components/video.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -10,12 +10,16 @@ const Video = ({ id, title, description, playlist_id, thumbnail_url }) => (
   <div className='o-grid__cell u-1/2@medium u-1/3@large u-1/4@extralarge'>
     <Link className='c-video' to={`/player/${playlist_id}/${id}`}>
       <div>
-        <div class="c-thumbnail">
-          <div class="c-thumbnail__link"></div>
-          <img class="c-thumbnail__img js-thumbnail-img" src={thumbnail_url} />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-poster="" class="c-thumbnail__play-icon"><path fill="currentColor" d="M1.425.35L14.575 8l-13.15 7.65V.35z"></path></svg>
-          <span class="c-thumbnail__progress-bar" style={progressBarWidth }></span>
-          <span class="c-thumbnail__duration-indicator">6:55</span>
+        <div class='c-thumbnail'>
+          <div class='c-thumbnail__link'></div>
+          <img class='c-thumbnail__img js-thumbnail-img' src={thumbnail_url} />
+          <div class='c-thumbnail__play-icon'>
+            <svg  width='16' height='22' viewBox='0 0 16 22' xmlns='http://www.w3.org/2000/svg'>
+              <path d='M14.837 11.818L1.575 21.142A1 1 0 0 1 0 20.324V1.676A1 1 0 0 1 1.575.858l13.262 9.324a1 1 0 0 1 0 1.636z' fill='currentColor' fill-rule='evenodd'/>
+            </svg>
+          </div>
+          <span class='c-thumbnail__progress-bar' style={progressBarWidth }></span>
+          <span class='c-thumbnail__duration-indicator'>6:55</span>
         </div>
         <h4 className='c-video__title'>{title}</h4>
       </div>
