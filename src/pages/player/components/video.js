ab513f3337f13cb6db831bbc0bc327bd746022ae
Nenad Jelovac <jelowack@gmail.com>
2018-10-14 12:39:31 +0200

    Add show/hide playlist button icon and styles

--- src/pages/player/components/video.js
+++ src/pages/player/components/video.js
@@ -7,7 +7,13 @@ const VideoPlayer = ({ playlistUrl, video, togglePlaylist }) => (
   <div className='video__player'>
     <div className='video__player__top'>
       <Link to={playlistUrl}>&#10005;</Link>
-      <button onClick={togglePlaylist}>playlist</button>
+      <button className='c-btn show-playlist' onClick={togglePlaylist}>
+        <svg className='o-icon' width='21' height='18' viewBox='0 0 21 18' xmlns='http://www.w3.org/2000/svg'>
+          <g stroke='currentColor' stroke-width='2' fill='none' fill-rule='evenodd' stroke-linecap='round' stroke-linejoin='round'>
+            <path d='M20 9H2M20 1H2M20 17H2M5 13L1 9l4-4'/>
+          </g>
+        </svg>
+      </button>
     </div>
 
     <div className='video__player__container'>
