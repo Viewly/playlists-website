46ecb54cd64373524e7ec58d64a70dcd514ba66c
Boris Vujicic <turshija@gmail.com>
2018-10-15 17:27:17 +0200

    Implement playing next video and going back to playlist when there is no next one

--- src/pages/player/index.js
+++ src/pages/player/index.js
@@ -34,6 +34,30 @@ class PlayerPage extends Component {
     this.setState({ showPlaylist: !this.state.showPlaylist });
   }
 
+  getNextVideoId = (videoId) => {
+    const { playlist } = this.props;
+    const currentVideo = playlist.videos.find(item => item.id === videoId);
+
+    if (currentVideo.position >= playlist.videos.length - 1) {
+      return -1;
+    } else {
+      const nextVideo = playlist.videos.find(item => item.position === currentVideo.position + 1);
+
+      return nextVideo.id;
+    }
+  }
+
+  onVideoEnd = () => {
+    const { history, playlist } = this.props;
+    const nextVideoId = this.getNextVideoId(this.state.videoId);
+
+    if (nextVideoId === -1) {
+      history.push(`/playlist/${playlist.id}`);
+    } else {
+      this.setState({ videoId: nextVideoId });
+    }
+  }
+
   render() {
     const { playlist } = this.props;
     const isLoaded = playlist._status === 'LOADED';
@@ -43,7 +67,7 @@ class PlayerPage extends Component {
 
     return (
       <Layout>
-        {isLoaded && <VideoPlayer togglePlaylist={this.togglePlaylist} playlistUrl={`/playlist/${playlist.id}`} video={currentVideo} />}
+        {isLoaded && <VideoPlayer togglePlaylist={this.togglePlaylist} playlistUrl={`/playlist/${playlist.id}`} video={currentVideo} onVideoEnd={this.onVideoEnd} />}
         {isLoaded && this.state.showPlaylist && <Playlist togglePlaylist={this.togglePlaylist} videos={playlist.videos} />}
       </Layout>
     );
