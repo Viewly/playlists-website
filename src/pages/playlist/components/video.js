a935699748c1fd03a7521b26c5354d03d67f119a
Nenad Jelovac <jelowack@gmail.com>
2018-10-15 12:20:18 +0200

    Replace class with className

--- src/pages/playlist/components/video.js
+++ src/pages/playlist/components/video.js
@@ -10,16 +10,16 @@ const Video = ({ id, title, description, playlist_id, thumbnail_url }) => (
   <div className='o-grid__cell u-1/2@medium u-1/3@large u-1/4@extralarge u-margin-bottom'>
     <Link className='c-video' to={`/player/${playlist_id}/${id}`}>
       <div>
-        <div class='c-thumbnail'>
-          <div class='c-thumbnail__link'></div>
-          <img class='c-thumbnail__img js-thumbnail-img' src={thumbnail_url} />
-          <div class='c-thumbnail__play-icon'>
+        <div className='c-thumbnail'>
+          <div className='c-thumbnail__link'></div>
+          <img className='c-thumbnail__img js-thumbnail-img' src={thumbnail_url} />
+          <div className='c-thumbnail__play-icon'>
             <svg  width='16' height='22' viewBox='0 0 16 22' xmlns='http://www.w3.org/2000/svg'>
               <path d='M14.837 11.818L1.575 21.142A1 1 0 0 1 0 20.324V1.676A1 1 0 0 1 1.575.858l13.262 9.324a1 1 0 0 1 0 1.636z' fill='currentColor' fill-rule='evenodd'/>
             </svg>
           </div>
-          <span class='c-thumbnail__progress-bar' style={progressBarWidth }></span>
-          <span class='c-thumbnail__duration-indicator'>6:55</span>
+          <span className='c-thumbnail__progress-bar' style={progressBarWidth }></span>
+          <span className='c-thumbnail__duration-indicator'>6:55</span>
         </div>
         <h4 className='c-video__title'>{title}</h4>
       </div>
