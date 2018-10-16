7b02d5e6666018ba9f50502ed395f776a43ac14c
Nenad Jelovac <jelowack@gmail.com>
2018-10-16 16:08:16 +0200

    Switch from animations to transitions

--- src/pages/player/components/playlist.js
+++ src/pages/player/components/playlist.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import PlaylistItem from "./playlist_item";
 
 const Playlist = ({ videos, togglePlaylist, isVisible }) => (
-  <div className={`c-player-playlist ${isVisible ? 'c-player-playlist--visible' : 'c-player-playlist--hidden'}`}>
+  <div className={`c-player-playlist ${isVisible ? 'is-visible' : ''}`}>
+
     <button className='c-btn c-player-playlist__btn-hide' onClick={togglePlaylist}>
-      <svg className='o-icon' width='24' height='18' viewBox='0 0 24 18' xmlns='http://www.w3.org/2000/svg'>
+      <svg className='o-icon' width='21' height='18' viewBox='0 0 21 18' xmlns='http://www.w3.org/2000/svg'>
         <g stroke='currentColor' strokeWidth='2' fill='none' fillRule='evenodd' strokeLinecap='round' strokeLinejoin='round'>
-          <path d='M23 9H13M23 1H5M23 17H5M5 13l4-4-4-4M9 9H1'/>
+          <path d='M20 9H2M20 1H2M20 17H2M5 13L1 9l4-4'/>
         </g>
       </svg>
     </button>
-
     <div className='c-player-playlist__content'>
       {videos && videos.map((item, idx) => <PlaylistItem key={`video-${idx}`} {...item} />)}
     </div>
--- src/pages/player/components/video.js
+++ src/pages/player/components/video.js
@@ -11,13 +11,6 @@ const VideoPlayer = ({ playlistUrl, video, togglePlaylist, onVideoEnd, onPercent
       </div>
 
       <Link className='c-btn button-back' to={playlistUrl}>&times;  </Link>
-      <button className='c-btn show-playlist' onClick={togglePlaylist}>
-        <svg className='o-icon' width='21' height='18' viewBox='0 0 21 18' xmlns='http://www.w3.org/2000/svg'>
-          <g stroke='currentColor' strokeWidth='2' fill='none' fillRule='evenodd' strokeLinecap='round' strokeLinejoin='round'>
-            <path d='M20 9H2M20 1H2M20 17H2M5 13L1 9l4-4'/>
-          </g>
-        </svg>
-      </button>
     </div>
 
   </div>
