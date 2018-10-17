5388cfe418fc26003ef06423e7c1d57c3233cfb0
Boris Vujicic <turshija@gmail.com>
2018-10-17 13:16:56 +0200

    Show real duration, category and number of videos on home page

--- src/pages/home/components/playlist_item.js
+++ src/pages/home/components/playlist_item.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const THUMBNAIL_ROOT = 'https://s3.eu-central-1.amazonaws.com/viewly-playlists-eu1/upload';
 const NO_THUMBNAIL = 'https://s3.eu-central-1.amazonaws.com/viewly-playlists-eu1/upload/a6a9391c-2f46-3e9c-d3ef-7af0ea2297a9_thumbnail.png';
 
-const PlaylistItem = ({ id, title, description, playlist_thumbnail_url }) => (
+const PlaylistItem = ({ id, title, description, playlist_thumbnail_url, duration, noVideos, category }) => (
   <div className='o-grid__cell u-1/2@medium u-1/3@large u-1/4@extralarge u-margin-bottom-large'>
 
     <div className='c-video'>
@@ -16,10 +16,10 @@ const PlaylistItem = ({ id, title, description, playlist_thumbnail_url }) => (
           : <img className='c-thumbnail__img' src={NO_THUMBNAIL} />
         }
         <p className='c-thumbnail__description c-video__description'>{description}</p>
-        <span className='c-thumbnail__total-videos'>6 videos</span>
-        <span className='c-thumbnail__duration-indicator'>6:55</span>
+        <span className='c-thumbnail__total-videos'>{noVideos} videos</span>
+        <span className='c-thumbnail__duration-indicator'>{duration}</span>
       </div>
-      <span className="c-video__category">Film &amp; Animation</span>
+      <span className="c-video__category">{category}</span>
       <h4 className='c-video__title c-video__title--large'><Link to={`/playlist/${id}`}>{title}</Link></h4>
       <div className='c-video__footer'>15.4k likes</div>
 
@@ -33,6 +33,9 @@ PlaylistItem.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
   playlist_thumbnail_url: PropTypes.string,
+  category: PropTypes.string,
+  noVideos: PropTypes.number,
+  duration: PropTypes.string,
 };
 
 export default PlaylistItem;
