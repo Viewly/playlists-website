c3d9768f8aa090ef04cc770f40499af0266e71a6
Boris Vujicic <turshija@gmail.com>
2018-10-18 13:28:36 +0200

    Create separate recommended item

--- src/pages/home/components/recommended.js
+++ src/pages/home/components/recommended.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import PlaylistItem from "./playlist_item";
+import RecommendedItem from "./recommended_item";
 
 const Playlist = ({ isLoaded, data }) => (
   <div>
     <h2>Staff picks</h2>
     <div className='o-grid'>
-      {isLoaded && data.map((item, idx) => <PlaylistItem key={`playlistitem-${idx}`} {...item} />)}
+      {isLoaded && data.map((item, idx) => <RecommendedItem key={`playlistitem-${idx}`} {...item} />)}
       {!isLoaded && <div>LOADING</div>}
     </div>
   </div>
