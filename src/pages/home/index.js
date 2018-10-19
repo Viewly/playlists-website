d7ac6bfc72f148aed012092d89dd04061544c392
Nenad Jelovac <jelowack@gmail.com>
2018-10-19 15:53:28 +0200

    Tweak buttons and copy on homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -34,14 +34,14 @@ class HomePage extends Component {
         <div className='c-hero'>
           <div className='o-wrapper'>
             <div className="c-hero">
-              <div className='o-grid'>
-                <div className='o-grid__cell c-hero__grid__cell u-2/3'>
+              <div className='o-grid o-grid--middle'>
+                <div className='o-grid__cell c-hero__grid__cell u-1/2'>
                   <h1 className="c-hero__title">Collaborative <br />YouTube playlists</h1>
-                  <p>Discover playlists, make your own, and contribute to others.</p>
+                  <p>Discover playlists, create your own, and contribute to others.</p>
                   <a href='#' className='c-btn c-btn--primary c-btn--large'>Create your playlist</a>
                 </div>
-                <div className='o-grid__cell c-hero__grid__cell u-1/3'>
-
+                <div className='o-grid__cell c-hero__grid__cell u-1/2'>
+                  <img src={require('../../images/hero-illustration.svg')} />
                 </div>
               </div>
             </div>
