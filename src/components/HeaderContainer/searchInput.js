a817446244fc793da47e28339b74c5fb9c2160c6
Nenad Jelovac <jelowack@gmail.com>
2018-10-22 15:07:05 +0200

    Style search box in header

--- src/components/HeaderContainer/searchInput.js
+++ src/components/HeaderContainer/searchInput.js
@@ -25,7 +25,18 @@ class SearchInput extends Component {
   render() {
     return (
       <Fragment>
-        <input className='c-input c-input--primary' placeholder="Search playlists" name="" autoComplete="off" value={this.state.searchText} onChange={(e) => this.setState({ searchText: e.target.value })} onKeyUp={this.handleEnter} />
+        <div className='c-search-form'>
+          <input className='c-search-form__input c-input c-input--primary' placeholder="Search playlists" name="" autoComplete="off" value={this.state.searchText} onChange={(e) => this.setState({ searchText: e.target.value })} onKeyUp={this.handleEnter} />
+          <button type='submit' className='c-btn c-search-form__btn'>
+            <svg className='o-icon' width="19" height="19" viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg">
+              <g transform="translate(1 1)" stroke="#9EA0A3" strokeWidth="2" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+                <circle cx="7.5" cy="7.5" r="7.5"/>
+                <path d="M17 17l-4.2-4.2"/>
+              </g>
+            </svg>
+
+          </button>
+        </div>
       </Fragment>
     );
   }
