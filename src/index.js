fa69f006e880d026fc41b742d673e8f93d1eaa77
Boris Vujicic <turshija@gmail.com>
2018-10-22 14:53:08 +0200

    Scroll to top every time route changes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./app";
+import ScrollToTop from "./components/scrollToTop";
 
 const $appContainer = document.getElementById("root");
 
 $appContainer && ReactDOM.render((
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </BrowserRouter>
   </Provider>), $appContainer
 );
