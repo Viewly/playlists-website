b036d0c58c008656f7c3a6a02e75a98e4dc150f3
Boris Vujicic <turshija@gmail.com>
2018-10-29 16:39:52 +0100

    generate right thumbnail url

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -19,13 +19,14 @@ class SEO extends Component {
         </MetaTags>
       )
     }
-
+    // TODO - url from constants or env
+    const poster = `https://s3.eu-central-1.amazonaws.com/viewly-playlists-eu1/upload/${playlist.playlist_thumbnail_url}`;
     return (
       <MetaTags>
         <title>{playlist.title} - {SEO_NAME}</title>
         <meta name="description" content={playlist.description} />
         <meta property="og:title" content={playlist.title} />
-        <meta property="og:image" content={playlist.thumbnail} />
+        <meta property="og:image" content={poster} />
         <meta property="og:url" content={playlist.url} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
