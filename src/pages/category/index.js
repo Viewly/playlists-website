4ca77db8cc946276ec9bf0401e5de77461c30331
Nenad Jelovac <jelowack@gmail.com>
2018-11-02 11:41:27 +0100

    Hide categories page until we have it set up properly

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -42,7 +42,7 @@ class CategoryPage extends Component {
 
     return (
       <div className='o-wrapper u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
-      <h1 className='u-h3'><Link to="/categories">Categories</Link> <span className='c-heading-delimiter'>&rsaquo;</span> {categoryId}</h1>
+      <h1 className='u-h3'>Categories <span className='c-heading-delimiter'>&rsaquo;</span> {categoryId}</h1>
 
         <Playlist title="" isLoaded={isReady} data={playlists.data.filter(i => i.category === categoryId)} onPlaylistClick={this.onPlaylistClick} />
       </div>
