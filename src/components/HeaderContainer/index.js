80dd246216d7439c0277c651cf5c1cbee3ff744f
Nenad Jelovac <jelowack@gmail.com>
2018-11-05 11:39:17 +0100

    Link up navigation and prevent jumps when switching pages

--- src/components/HeaderContainer/index.js
+++ src/components/HeaderContainer/index.js
@@ -21,15 +21,15 @@ class HeaderContainer extends Component {
             <nav className='c-nav-primary'>
               <ul>
                 <li className='c-nav-primary__item is-current'>
-                  <a href='' className='c-nav-primary__link'>
+                  <Link to='/' className='c-nav-primary__link'>
                     <svg className='o-icon' width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                       <path d='M8.667 23v-9.9h6.666V23H20a2 2 0 0 0 2-2V8.7L12 1 2 8.7V21a2 2 0 0 0 2 2h4.667z' stroke='currentColor' strokeWidth='2' fill='none' strokeLinecap='round' strokeLinejoin='round'/>
                     </svg>
                     <span className='c-nav-primary__label'>Home</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className='c-nav-primary__item'>
-                  <a href='' className='c-nav-primary__link'>
+                  <Link to='/new' className='c-nav-primary__link'>
                     <svg className='o-icon' width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                       <g transform='translate(1 1)' stroke='currentColor' strokeWidth='2' fill='none' fillRule='evenodd' strokeLinecap='round' strokeLinejoin='round'>
                         <circle cx='11' cy='11' r='11'/>
@@ -37,17 +37,17 @@ class HeaderContainer extends Component {
                       </g>
                     </svg>
                     <span className='c-nav-primary__label'>New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className='c-nav-primary__item'>
-                  <a href='' className='c-nav-primary__link'>
+                  <Link to='/categories' className='c-nav-primary__link'>
                     <svg className='o-icon' width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
                       <g stroke='currentColor' strokeWidth='2' fill='none' fillRule='evenodd' strokeLinecap='round' strokeLinejoin='round'>
                         <path d='M1 1h7v7H1zM12 1h7v7h-7zM12 12h7v7h-7zM1 12h7v7H1z'/>
                       </g>
                     </svg>
                     <span className='c-nav-primary__label'>Categories</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
