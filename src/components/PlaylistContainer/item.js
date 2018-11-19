2678c443a80046e615f02ce1a132bf931b31322d
Boris Vujicic <turshija@gmail.com>
2018-11-19 18:45:41 +0100

    Adding and removing bookmarks

--- src/components/PlaylistContainer/item.js
+++ src/components/PlaylistContainer/item.js
@@ -3,9 +3,8 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { THUMBNAIL_ROOT, NO_THUMBNAIL } from "../../constants";
 
-const PlaylistItem = ({ url, title, description, playlist_thumbnail_url, duration, noVideos, category, onPlaylistClick, big }) => (
+const PlaylistItem = ({ id, url, title, description, playlist_thumbnail_url, duration, noVideos, category, onPlaylistClick, onBookmarkClick, big }) => (
   <div className={`o-grid__cell u-1/2@medium u-1/3@large ${big ? "" : "u-1/4@extralarge"} u-margin-bottom-large`} onClick={() => onPlaylistClick(url)}>
-
     <div className='c-video'>
       <div className='c-thumbnail'>
         <Link onClick={onPlaylistClick(url)} className='c-thumbnail__link' to={`/playlist/${url}`}></Link>
@@ -17,6 +16,7 @@ const PlaylistItem = ({ url, title, description, playlist_thumbnail_url, duratio
         <span className='c-thumbnail__total-videos'>{noVideos} videos</span>
         <span className='c-thumbnail__duration-indicator'>{duration}</span>
       </div>
+      <div className="c-video__bookmark" onClick={() => onBookmarkClick(id)}>❤️</div>
       <span className="c-video__category"><Link to={`/category/${category.slug}`}>{category.name}</Link></span>
       <h4 className='c-video__title c-video__title--large'><Link onClick={onPlaylistClick(url)} to={`/playlist/${url}`}>{title}</Link></h4>
     </div>
@@ -34,6 +34,7 @@ PlaylistItem.propTypes = {
   noVideos: PropTypes.number,
   duration: PropTypes.string,
   onPlaylistClick: PropTypes.func,
+  onBookmarkClick: PropTypes.func,
   big: PropTypes.bool,
 };
 
