d663cf4e99275f488971b8e383ef2edfc6767f22
Boris Vujicic <turshija@gmail.com>
2018-11-19 15:00:56 +0100

    Moved auth sidebar to separate component

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import AuthSidebar from "../../components/authSidebar";
 import { userRegister, LOGIN_SUCCESS_PERSIST } from "../../actions/user";
 
 @connect(null, (dispatch) => ({
@@ -45,18 +46,7 @@ class RegistrationPage extends Component {
   render() {
     return (
       <div className='c-auth'>
-        <div className='c-auth__sidebar'>
-          <Link to='/' className='c-logo c-auth__logo'>
-            <img className='c-logo__img' src={require("../../images/logo-white.svg")} />
-          </Link>
-          <div className='c-auth__sidebar__content'>
-            <h1 className='c-auth__sidebar__title'>Collaborative YouTube playlists</h1>
-            <p>
-              Discover playlists, create your own,
-              and contribute to others.
-            </p>
-          </div>
-        </div>
+        <AuthSidebar />
 
         <div className='c-auth__main'>
 
