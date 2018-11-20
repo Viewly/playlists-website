db45e614d3def454797a1bda44c806fbb1d83f78
Nenad Jelovac <jelowack@gmail.com>
2018-11-20 11:04:08 +0100

    Style password reset instructions sent page

--- src/pages/reset-password/components/requested.js
+++ src/pages/reset-password/components/requested.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import AuthSidebar from "../../../components/authSidebar";
 
 class ResetRequested extends Component {
   render() {
     return (
-      <div className='o-wrapper o-wrapper--narrow u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
-        Please check your email for further instructions
+      <div className='c-auth'>
+        <AuthSidebar />
+
+        <div className='c-auth__main'>
+
+          <div className='c-auth__main__content'>
+            <div className='c-thank-you'>
+              <img className='c-thank-you__img' src={require("../../../images/graphic-success.svg")} />
+              <h1 className='u-h3 u-margin-bottom-tiny'>Instructions successfully sent</h1>
+              <p>Please check your email for further instructions.</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
