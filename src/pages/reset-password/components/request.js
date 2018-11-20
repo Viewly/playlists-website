900a475fd57ced7be0dec3d008a3c68339c14ad9
Nenad Jelovac <jelowack@gmail.com>
2018-11-20 10:51:53 +0100

    Style forgot password request page

--- src/pages/reset-password/components/request.js
+++ src/pages/reset-password/components/request.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 
+import AuthSidebar from "../../../components/authSidebar";
 import { userForgotPassword } from "../../../actions/user";
 
 @withRouter
@@ -42,32 +43,45 @@ class ResetRequest extends Component {
 
   render() {
     return (
-      <div className='o-wrapper o-wrapper--narrow u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
+      <div className='c-auth'>
+        <AuthSidebar />
 
-        {this.state.error && (
-          <div className='c-thank-you'>
-            <h5 className='u-margin-bottom-small'>An error occurred</h5>
-            <p>{this.state.errorText}</p>
+        <div className='c-auth__main'>
+
+          <div className='c-auth__main__content'>
+
+            <div className='c-auth__main__header'>
+              <h1 className='u-h3 u-margin-bottom-tiny'>Forgot password?</h1>
+              <p>Don't worry! Enter your email address and we'll get you back on track.</p>
+            </div>
+
+            {this.state.error && (
+              <div className=''>
+                <h5 className='u-margin-bottom-small'>An error occurred</h5>
+                <p>{this.state.errorText}</p>
+              </div>
+            )}
+
+            <form className='c-form' onSubmit={this.handleSubmit}>
+              <ul className='c-form__list'>
+                <li>
+                  <label className='c-form__label'>Email address</label>
+                  <input className='c-input c-input--primary' type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                </li>
+
+                <li>
+                  <div className='o-grid o-grid--middle o-grid--auto o-grid--between'>
+                    <div className='o-grid__cell'>
+                      <Link to='/login'>&larr; Back to login</Link>
+                    </div>
+                    <div className='o-grid__cell'>
+                      <button className='c-btn c-btn--primary'>Send email</button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </form>
           </div>
-        )}
-
-        <div>
-          <h1 className='c-form__title'>Forgot password</h1>
-
-          <form className='c-form' onSubmit={this.handleSubmit}>
-            <ul className='c-form__list'>
-              <li>
-                <label className='c-form__label'>Email address</label>
-                <input className='c-input c-input--primary' type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-              </li>
-
-              <li className='u-text-right'>
-                <button className='c-btn c-btn--primary'>Send email</button>
-              </li>
-
-              <p><Link to='/login'>Back to login</Link></p>
-            </ul>
-          </form>
         </div>
 
       </div>
