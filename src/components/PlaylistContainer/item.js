eaac08c53fb014e792031962e3cf45a7f1b0ace7
Nenad Jelovac <jelowack@gmail.com>
2018-11-28 12:52:38 +0100

    Add bookmarked indicator

--- src/components/PlaylistContainer/item.js
+++ src/components/PlaylistContainer/item.js
@@ -15,8 +15,10 @@ const PlaylistItem = ({ id, url, title, description, playlist_thumbnail_url, dur
         <p className='c-thumbnail__description c-video__description'>{description}</p>
         <span className='c-thumbnail__total-videos'>{noVideos} videos</span>
         <span className='c-thumbnail__duration-indicator'>{duration}</span>
+        <div className="c-thumbnail__bookmark">
+          <img src={require("../../images/icons/bookmarked-indicator.svg")} />
+        </div>
       </div>
-      {onBookmarkClick && <div className="c-video__bookmark" onClick={() => onBookmarkClick(id)}>❤️</div>}
       <span className="c-video__category"><Link to={`/category/${category.slug}`}>{category.name}</Link></span>
       <h4 className='c-video__title c-video__title--large'><Link onClick={onPlaylistClick(url)} to={`/playlist/${url}`}>{title}</Link></h4>
     </div>
