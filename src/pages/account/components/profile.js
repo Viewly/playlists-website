1fb2c628adfa054dfa10998c3d6a9fa293b4b10f
Boris Vujicic <turshija@gmail.com>
2018-11-28 16:16:25 +0100

    Turned account profile into form

--- src/pages/account/components/profile.js
+++ src/pages/account/components/profile.js
@@ -14,27 +14,70 @@ class UserProfile extends Component {
     user: PropTypes.object
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: props.user.email,
+      first_name: props.user.first_name,
+      last_name: props.user.last_name,
+      email_confirmed: props.user.email_confirmed,
+      avatar_url: props.user.avatar_url
+    };
+  }
+
   confirmationEmail = () => {
     const { user, userEmailRequest } = this.props;
     userEmailRequest(user.email);
   }
 
-  render() {
-    const { user } = this.props;
+  handleChange = (evnt) => {
+    const field = evnt.target.name;
 
+    this.setState({ [field]: evnt.target.value });
+  }
+
+  handleSubmit = async (evnt) => {
+    evnt.preventDefault();
+    alert("SAVE");
+  }
+
+  render() {
     return (
-      <div className='o-wrapper u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
+      <div className='o-wrapper o-wrapper--narrow u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
         <h1 className='u-h3'>My profile</h1>
 
-        Email: {user.email}
-        <br />
-        First name: {user.first_name}
-        <br />
-        Last name: {user.last_name}
-        <br />
-        {!user.email_confirmed && (
+        {!this.state.email_confirmed && (
           <div>Email is NOT confirmed <button onClick={this.confirmationEmail}>Send confirmation email</button></div>
         )}
+
+        <div style={{ maxWidth: "200px" }}>
+          <img src={this.state.avatar_url} />
+          <button>Upload image</button>
+          <p>JPG, GIF or PNG. Max size of 800KB</p>
+        </div>
+
+        <form className='c-form' onSubmit={this.handleSubmit}>
+          <ul className='c-form__list c-form__list--large'>
+            <li>
+              <label className='c-form__label'>First name</label>
+              <input className='c-input c-input--primary' type="text" name="first_name" value={this.state.first_name} onChange={this.handleChange} />
+            </li>
+            <li>
+              <label className='c-form__label'>Last name</label>
+              <input className='c-input c-input--primary' type="text" name="last_name" value={this.state.last_name} onChange={this.handleChange} />
+            </li>
+
+            <li>
+              <label className='c-form__label'>Your email address</label>
+              <input className='c-input c-input--primary' type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+            </li>
+            <li className='u-text-right'>
+              <button className='c-btn c-btn--primary'>Save Changes</button>
+            </li>
+          </ul>
+        </form>
+
       </div>
     );
   }
