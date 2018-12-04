64f1890dd65bb5c19e00ae57e0ed0b66c7b8c746
Nenad Jelovac <jelowack@gmail.com>
2018-12-04 15:59:37 +0100

    Skip forgot password link with tabs

--- src/pages/account/components/password.js
+++ src/pages/account/components/password.js
@@ -68,7 +68,7 @@ class UserPassword extends Component {
               <label className='c-form__label'>Current password</label>
               <input className='c-input c-input--primary' type="password" name="current_password" value={this.state.current_password} onChange={this.handleChange} />
               <div className='c-annotation u-text-right u-margin-top-tiny'>
-                <Link to='/reset-password'>Forgot current password?</Link>
+                <Link to='/reset-password' tabindex="-1">Forgot current password?</Link>
               </div>
             </li>
             <li>
