7b7411a824a5a2224a8cb58759a996e048b26d7a
Nenad Jelovac <jelowack@gmail.com>
2018-12-07 22:59:28 +0100

    Add upload indicator style, and image uploaded view

--- src/pages/create-playlist/index.js
+++ src/pages/create-playlist/index.js
@@ -86,11 +86,13 @@ class CreatePlaylist extends Component {
                     <div className='c-file-input__container o-ratio__content'>
                       <input className='c-file-input__input' type="file" id="file" />
                       <div className='c-file-input__content'>
-                        <img className='c-file-input__img' src={require("../../images/graphic-add-photo.png")} />
+                        <img className='c-file-input__graphic' src={require("../../images/graphic-add-photo.png")} />
                         <p>Drag and drop or <span>browse</span> <br />for the thumbnail to upload</p>
                         <small className='c-file-input__annotation'>JPG, GIF or PNG. Max size 5mb</small>
                       </div>
                     </div>
+                    <span className='c-file-input__upload-progress' style={{ width: '55%' }}></span>
+                    {/*<img className='o-ratio__content' src='https://media.vidflow.io/upload/26940115-7c11-0dcf-34de-f2e52566c7bf_thumbnail.jpg' />*/}
                   </label>
                 </li>
 
