c86fb4ffffde9837b3abc12b61598b1bbd2487a2
Nenad Jelovac <jelowack@gmail.com>
2018-12-07 18:37:29 +0100

    Style playlist builder first pass

--- src/pages/create-playlist/index.js
+++ src/pages/create-playlist/index.js
@@ -44,90 +44,239 @@ class CreatePlaylist extends Component {
     const { categories } = this.props;
 
     return (
-      <div className='o-wrapper o-wrapper--narrow u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
+      <div className='o-wrapper u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
 
         <form onSubmit={this.handleSubmit}>
+          <div className="o-grid o-grid--center o-grid--large">
 
-          <div className="o-grid o-grid--center o-grid--huge o-grid--auto">
+            <div className='o-grid__cell u-4/5@medium u-1/2@large u-2/5@extralarge'>
+              <ul className='c-form__list c-form__list--large'>
+                <li>
+                  <label className='c-form__label'>Playlist name</label>
+                  <input
+                    className='c-input c-input--primary'
+                    type="text"
+                    name="playlist_name"
+                    value={this.state.playlist_name}
+                    onChange={this.handleChange}
+                    required/>
+                </li>
 
-            <ul className='o-grid__cell c-form__list c-form__list--large'>
-              <li>
-                <label className='c-form__label'>Playlist name</label>
-                <input
-                  className='c-input c-input--primary'
-                  type="text"
-                  name="playlist_name"
-                  value={this.state.playlist_name}
-                  onChange={this.handleChange}
-                  required/>
-              </li>
+                <li>
+                  <label className='c-form__label'>Category</label>
+                  <div className='c-select u-1/1'>
+                    <select
+                      className='c-select__select'
+                      name="category"
+                      value={this.state.category}
+                      onChange={this.handleChange}>
 
-              <li>
-                <label className='c-form__label'>Playlist thumbnail</label>
-                <div>
-                  drag and drop here or browse
-                </div>
-              </li>
+                      <option value="0">No category</option>
+                      {categories.data.map((item, idx) => (
+                        <option key={`category-${idx}`} value={item.id}>{item.name}</option>
+                      ))}
 
-              <li>
-                <label className='c-form__label'>Category</label>
-                <div className='c-select u-1/1'>
-                  <select
-                    className='c-select__select'
-                    name="category"
-                    value={this.state.category}
-                    onChange={this.handleChange}>
+                    </select>
+                  </div>
+                </li>
 
-                    <option value="0">No category</option>
-                    {categories.data.map((item, idx) => (
-                      <option key={`category-${idx}`} value={item.id}>{item.name}</option>
-                    ))}
+                <li>
+                  <label className='c-form__label'>Playlist thumbnail</label>
+                  <div>
+                    drag and drop here or browse
+                  </div>
+                </li>
 
-                  </select>
-                </div>
-              </li>
+                <li>
+                  <label className='c-form__label'>Description</label>
+                  <textarea
+                    className='c-input c-input--primary c-input--textarea'
+                    type="text"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    required></textarea>
+                </li>
 
-              <li>
-                <label className='c-form__label'>Hashtags</label>
-                <input
-                  className='c-input c-input--primary'
-                  type="text"
-                  name="hashtags"
-                  value={this.state.hashtags}
-                  onChange={this.handleChange}
-                  required/>
-              </li>
+                <li>
+                  <label className='c-form__label'>Hashtags</label>
+                  <input
+                    className='c-input c-input--primary'
+                    type="text"
+                    name="hashtags"
+                    value={this.state.hashtags}
+                    onChange={this.handleChange}
+                    required/>
+                </li>
+              </ul>
+            </div>
 
-              <li className='u-text-right'>
-                <button className='c-btn c-btn--secondary'>Next</button>
-              </li>
-            </ul>
+            <div className='o-grid__cell u-4/5@medium u-1/2@large u-2/5@extralarge u-margin-top-large u-margin-top-none@large'>
+              <ul className='c-form__list c-form__list--large'>
+                <li>
+                  <label className='c-form__label'>Add videos</label>
+                  <div className='o-flag o-flag--reverse'>
+                    <div className='o-flag__img'>
+                      <button className='c-btn c-btn--secondary c-btn--square'>
+                        <img className='o-icon o-icon--small' src={require("../../images/icons/plus.svg")} />
+                      </button>
+                    </div>
+                    <div className='o-flag__body'>
+                      <input
+                        className='c-input c-input--primary'
+                        type="text"
+                        name=""
+                        value=""
+                        onChange={this.handleChange}
+                        required/>
+                    </div>
+                  </div>
+                  <small className='c-form__annotation'>Copy video URL and paste it here</small>
+                </li>
+              </ul>
+              <ul className='u-margin-top-large c-form__list c-form__list--small'>
+                <li>
+                  <div className='c-video-link-preview'>
+                    <div className='o-grid o-grid--auto o-grid--nowrap o-grid--middle o-grid--small'>
 
-            <ul className='o-grid__cell c-form__list c-form__list--large'>
-              <li>
-                <label className='c-form__label'>Playlist name</label>
-                <input
-                  className='c-input c-input--primary'
-                  type="text"
-                  name="playlist_name"
-                  value={this.state.playlist_name}
-                  onChange={this.handleChange}
-                  required/>
-              </li>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/reorder.svg")} />
+                        </div>
+                      </div>
+                      <div className='o-grid__cell o-grid__cell--grow'>
+                        <div className='o-grid o-grid--small o-grid--auto o-grid--nowrap o-grid--middle'>
+                          <div className='o-grid__cell u-1/3'>
+                            <span className='o-ratio o-ratio--16:9'>
+                              <img className='c-video-link-preview__thumbnail o-ratio__content' src='https://i.ytimg.com/vi/fFcml2J5ElE/mqdefault.jpg'/>
+                            </span>
+                          </div>
+                          <div className='o-grid__cell u-2/3'>
+                            <input
+                              className='c-input c-input--primary c-input--small'
+                              type="text"
+                              name=""
+                              value='Premier League Preview - GW 13'
+                              required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/delete.svg")} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className='c-video-link-preview'>
+                    <div className='o-grid o-grid--auto o-grid--nowrap o-grid--middle o-grid--small'>
 
-              <li>
-                video
-              </li>
-              <li>
-                video
-              </li>
-              <li>
-                video
-              </li>
-              <li>
-                video
-              </li>
-            </ul>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/reorder.svg")} />
+                        </div>
+                      </div>
+                      <div className='o-grid__cell o-grid__cell--grow'>
+                        <div className='o-grid o-grid--small o-grid--auto o-grid--nowrap o-grid--middle'>
+                          <div className='o-grid__cell u-1/3'>
+                            <span className='o-ratio o-ratio--16:9'>
+                              <img className='c-video-link-preview__thumbnail o-ratio__content' src='https://i.ytimg.com/vi/fFcml2J5ElE/mqdefault.jpg'/>
+                            </span>
+                          </div>
+                          <div className='o-grid__cell u-2/3'>
+                            <input
+                              className='c-input c-input--primary c-input--small'
+                              type="text"
+                              name=""
+                              value='Premier League Preview - GW 13'
+                              required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/delete.svg")} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className='c-video-link-preview'>
+                    <div className='o-grid o-grid--auto o-grid--nowrap o-grid--middle o-grid--small'>
+
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/reorder.svg")} />
+                        </div>
+                      </div>
+                      <div className='o-grid__cell o-grid__cell--grow'>
+                        <div className='o-grid o-grid--small o-grid--auto o-grid--nowrap o-grid--middle'>
+                          <div className='o-grid__cell u-1/3'>
+                            <span className='o-ratio o-ratio--16:9'>
+                              <img className='c-video-link-preview__thumbnail o-ratio__content' src='https://i.ytimg.com/vi/fFcml2J5ElE/mqdefault.jpg'/>
+                            </span>
+                          </div>
+                          <div className='o-grid__cell u-2/3'>
+                            <input
+                              className='c-input c-input--primary c-input--small'
+                              type="text"
+                              name=""
+                              value='Premier League Preview - GW 13'
+                              required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/delete.svg")} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className='c-video-link-preview'>
+                    <div className='o-grid o-grid--auto o-grid--nowrap o-grid--middle o-grid--small'>
+
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/reorder.svg")} />
+                        </div>
+                      </div>
+                      <div className='o-grid__cell o-grid__cell--grow'>
+                        <div className='o-grid o-grid--small o-grid--auto o-grid--nowrap o-grid--middle'>
+                          <div className='o-grid__cell u-1/3'>
+                            <span className='o-ratio o-ratio--16:9'>
+                              <img className='c-video-link-preview__thumbnail o-ratio__content' src='https://i.ytimg.com/vi/fFcml2J5ElE/mqdefault.jpg'/>
+                            </span>
+                          </div>
+                          <div className='o-grid__cell u-2/3'>
+                            <input
+                              className='c-input c-input--primary c-input--small'
+                              type="text"
+                              name=""
+                              value='Premier League Preview - GW 13'
+                              required/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='o-grid__cell'>
+                        <div className='c-faux-link'>
+                          <img className='o-icon o-icon--small' src={require("../../images/icons/delete.svg")} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='u-text-right u-horizontally-center u-margin-top u-margin-top-large@large u-4/5@medium u-1/1@large u-4/5@extralarge'>
+            <button className='c-btn c-btn--secondary c-btn--hollow u-margin-right-small'>Save as draft</button>
+            <button className='c-btn c-btn--secondary'>Publish</button>
           </div>
         </form>
 
