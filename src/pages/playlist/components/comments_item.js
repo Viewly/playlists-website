a5fc5b4cf3a84fdb60fac4e9add06a6d43b1fe01
Boris Vujicic <turshija@gmail.com>
2018-12-19 00:34:09 +0100

    Implement avatars in comments

--- src/pages/playlist/components/comments_item.js
+++ src/pages/playlist/components/comments_item.js
@@ -10,11 +10,12 @@ export default class PlaylistCommentItem extends Component {
   render() {
     const { user, description, created_at } = this.props;
     const timeAgo = moment(created_at).startOf("minute").fromNow();
-    
+
     return (
       <div className='o-flag o-flag--small u-margin-top'>
         <div className='o-flag__img'>
-          <img className='o-avatar o-avatar--large' src={require("../../../images/avatar-default.jpg")}/>
+          {user.avatar_url && <img alt='' className='o-avatar o-avatar--large' src={user.avatar_url} />}
+          {!user.avatar_url && <img alt='' className='o-avatar o-avatar--large' src={require("../../../images/avatar-default.jpg")}/>}
         </div>
         <div className='o-flag__body'>
           <span><b>{user?.alias}</b> <time className='c-time'>{timeAgo}</time></span>
