7aaf739440671e1c883c58aae096020aced09577
Boris Vujicic <turshija@gmail.com>
2019-01-09 14:29:05 +0100

    Changed the order of commands in run ssr server script

--- run-ssr-server.sh
+++ run-ssr-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p dist
+npm run ssr-build
 aws s3 cp s3://viewly-playlists-website/index.html dist/
 aws s3 cp s3://viewly-playlists-website/favicon.ico dist/
-npm run ssr-build
 npm run ssr-start
