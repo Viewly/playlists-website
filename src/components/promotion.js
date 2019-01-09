08724e8699ccf5bd69587d0069e3182ecb868cdb
Nenad Jelovac <jelowack@gmail.com>
2019-01-09 22:12:23 +0100

    Style promo message close button

--- src/components/promotion.js
+++ src/components/promotion.js
@@ -17,8 +17,8 @@ export default class Promotion extends Component {
           <div className='c-promotion-message__text'>
             Help us make VidFlow better and earn 1 year premium membership!
             <Link to='/promo-early-adopters'>Learn more</Link>
-            <button onClick={() => { hidePromotion() }}>dismiss</button>
           </div>
+          <button className='c-btn c-promotion-message__btn-close' onClick={() => { hidePromotion() }}>&times;</button>
         </div>
       </div>
     );
