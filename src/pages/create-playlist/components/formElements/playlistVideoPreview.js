d9c3d5fc9a70233ac016649ceeba013d0923082f
Nenad Jelovac <jelowack@gmail.com>
2019-01-18 15:13:19 +0100

    Add set thumb button and improve responsive

--- src/pages/create-playlist/components/formElements/playlistVideoPreview.js
+++ src/pages/create-playlist/components/formElements/playlistVideoPreview.js
@@ -39,13 +39,14 @@ export default class PlaylistVideoPreview extends Component {
               src={require("../../../../images/icons/reorder.svg")}/>
           </div>
           <div className='o-grid__cell o-grid__cell--grow'>
-            <div className='o-grid o-grid--small o-grid--auto o-grid--nowrap o-grid--middle'>
-              <div className='o-grid__cell u-1/3'>
-                <span className='o-ratio o-ratio--16:9'>
-                  <img className='c-video-link-preview__thumbnail o-ratio__content' src={thumbnail_url}/>
+            <div className='o-grid o-grid--small o-grid--auto o-grid--nowap o-grid--middle'>
+              <div className='o-grid__cell u-1/1 u-1/3@medium'>
+                <span className='c-video-link-preview__thumbnail o-ratio o-ratio--16:9'>
+                  <img className='o-ratio__content' src={thumbnail_url}/>
+                  <button className='c-btn c-btn--plain c-btn--tiny c-video-link-preview__thumbnail__btn' href='/'>set as thumbnail</button>
                 </span>
               </div>
-              <div className='o-grid__cell u-2/3'>
+              <div className='o-grid__cell u-1/1 u-2/3@medium u-margin-top-small u-margin-top-none@medium'>
                 <input
                   className='c-input c-input--primary c-input--small'
                   type="text"
