bbbb01dfc731898ce952063422769f074c73bed7
Nenad Jelovac <jelowack@gmail.com>
2019-01-23 13:51:54 +0100

    Redirect homepage cta button to login

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -69,7 +69,7 @@ class HomePage extends Component {
                 <div className='o-grid__cell c-hero__grid__cell'>
                   <h1 className="c-hero__title">Collaborative <br />YouTube playlists</h1>
                   <p>Discover playlists, create your own, and contribute to others.</p>
-                  <Link to='/create-playlist' className='c-btn c-btn--primary c-btn--large'>Create your playlist</Link>
+                  <Link to='/register' className='c-btn c-btn--primary c-btn--large'>Create your playlist</Link>
                 </div>
                 <div className='o-grid__cell c-hero__grid__cell'>
                   <img alt='' className='c-hero__graphic' src={require("../../images/hero-illustration.svg")} />
