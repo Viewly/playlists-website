7321c8658d8527159df858f6d595d9de47c2e042
Boris Vujicic <turshija@gmail.com>
2019-01-24 16:07:53 +0100

    Replace iframe with JS library

--- src/pages/giveaway/index.js
+++ src/pages/giveaway/index.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import SEO from "../../components/SEO";
 
 export default class GiveawayPage extends Component {
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "https://js.gleam.io/e.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div className='o-wrapper u-padding-top-large u-padding-top-huge@large u-padding-bottom'>
         <SEO title="Giveaway" />
-
-        <iframe style={{ width: '100%', height: '100vh' }} src="https://gleam.io/K1ioX/iberleezy-x-vidflow-giveaway" frameBorder="0" allowFullScreen />
+        
+        <a className="e-widget no-button generic-loader" href="https://gleam.io/K1ioX/iberleezy-x-vidflow-giveaway" rel="nofollow">iBerleezy x Vidflow Giveaway</a>
       </div>
     );
   }
